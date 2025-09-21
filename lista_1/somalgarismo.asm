5429ee83877cef6f36ef93313e275beb55809594
Arthur da Silva Mariz <85581944+arthursm-aqb@users.noreply.github.com>
2025-09-21 16:48:54 -0300

    Update somalgarismo.asm

--- lista_1/somalgarismo.asm
+++ lista_1/somalgarismo.asm
@@ -1,15 +1,17 @@
 .text 
+
+#Fa√ßa um programa que leia um n√∫mero inteiro entre 0 e 999 e imprima a soma dos algarismos desse n√∫mero. Ex.: 358 gera uma sa√≠da de 16, pois 3+5+8 = 16
 main:
 	addi $v0, $zero, 5
 	syscall
-	addi $t0, $zero, 10 #T0 … IGUAL A 10 e È o divisor
+	addi $t0, $zero, 10 #T0 √â IGUAL A 10 e √© o divisor
 	addi $t2, $zero, 0
-	add $t1, $zero, $v0 #T1 armazenar· o didivendo
+	add $t1, $zero, $v0 #T1 armazenar√° o didivendo
 	div $t1, $t0 #Vamos somar LO com o valor de T2
 	mflo $t1
 	mfhi $t2
 	div $t1, $t0
-	mfhi $t3 #T3 ser· a vari·vel auxiliar
+	mfhi $t3 #T3 ser√° a vari√°vel auxiliar
 	add $t2, $t2, $t3
 	mflo $t1
 	div $t1, $t0
