9c84bc69f8646e5db2bc01ddb721c5683f92b704
Arthur da Silva Mariz <85581944+arthursm-aqb@users.noreply.github.com>
2025-09-25 18:46:59 -0300

    Delete lista_1/dobrar.asm

--- lista_1/dobrar.asm
+++
@@ -1,9 +0,0 @@
-.text 
-#Faça um programa para ler um número inteiro e imprimir o dobro desse número.
-main:
-	add $v0, $zero, 5
-	syscall
-	add $t0, $zero, $v0
-	sll $a0, $t0, 1
-	add $v0, $zero, 1
-	syscall
